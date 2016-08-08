c76c39bf928c4f69fbe04a17b4b2820646a0ea4d
C. Augusto Suarez <augustosuarez22@gmail.com>
2016-08-08 00:45:02 -0400

    bawbouwsz first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!-- leave just Name priority and subtasks, and add a more button, which shows due date, other description -->
+<head>
+<?php
+
+?>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="stylesheet.css">
+<script src="js/jquery.min.js"></script>
+</head>
+<html>
+
+</html>
